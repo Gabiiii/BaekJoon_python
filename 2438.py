a3b40820800a722abe41b01c312154211f6a64f8
Gabiiii <anna9570@gmail.com>
2018-10-24 16:02:54 +0900

    2438 별찍기-1

---
+++ 2438.py
@@ -0,0 +1,11 @@
+# 2438 º°Âï±â-1
+inp = int(input())
+for i in range(1,(inp+1)):
+    print("*" * i)
+'''
+inp = int(input())
+for i in range(1, (inp+1)):
+    for j in range(1, (i+1)):
+        print("*",end="")
+    print()
+'''
