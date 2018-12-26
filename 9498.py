d8a321acd43ac441112c105e54c89b16a19f9769
Gabiiii <anna9570@gmail.com>
2018-12-26 09:42:08 +0900

    9498 시험 성적

---
+++ 9498.py
@@ -0,0 +1,12 @@
+# 9498 시험 성적
+inp = int(input())
+if inp >= 90 and inp <=100:
+        print("A")
+elif inp >= 80 and inp <= 89:
+        print("B")
+elif inp >= 70 and inp <= 79:
+        print("C")
+elif inp >= 60 and inp <= 69:
+        print("D")
+else:
+        print("F")
