79f92d3b09c9fb76b8b5bf791f34ab263402e6c8
Gabiiii <anna9570@gmail.com>
2018-12-31 15:18:54 +0900

    10871 X보다 작은 수

---
+++ 10871.py
@@ -0,0 +1,6 @@
+# 10871 X보다 작은 수
+count, num = map(int, input().split())
+inArr = list(map(int, input().split()))
+for i in range(count):
+        if inArr[i] < num:
+                print(inArr[i], end=" ")
