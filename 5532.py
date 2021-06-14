d864cac4c07bd1367bb5fa5ebc287a503f4351db
Gabiiii <anna9570@gmail.com>
2021-06-14 13:11:43 +0900

    5532 방학 숙제

---
+++ 5532.py
@@ -0,0 +1,20 @@
+l=int(input())
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+
+if a%c==0:
+    kor=a//c
+else:
+    kor=(a//c)+1
+
+if b%d==0:
+    math=b//d
+else:
+    math=(b//d)+1
+
+if kor>math:
+    print(l-kor)
+else:
+    print(l-math)
