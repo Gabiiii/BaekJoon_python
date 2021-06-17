e013c7a4def8ace6ddf9eb0afd836b9240c53e58
Gabiiii <anna9570@gmail.com>
2021-06-17 17:49:39 +0900

    1546 평균

--- 1546.py
+++ 1546.py
@@ -1,14 +1,14 @@
-# 1546 Æò±Õ 
+# 1546 ï¿½ï¿½ï¿½ 
 num = int(input())
 max = 0
 avg = 0
 inArr = list(map(float, input().split()))
 for i in range(num):
-        if max < inArr[i]:
-                max = inArr[i]
+    if max < inArr[i]:
+        max = inArr[i]
 
 for i in range(num):
-        inArr[i] = inArr[i]/max*100.0
-        avg += inArr[i]
+    inArr[i] = inArr[i]/max*100.0
+    avg += inArr[i]
 
 print(round(avg/num, 2))
