7e1635560612022e4293e648801eea142b0a2edc
Gabiiii <anna9570@gmail.com>
2021-06-17 15:30:27 +0900

    2446 별 찍기-9

---
+++ 2446.py
@@ -0,0 +1,9 @@
+a = int(input())
+
+for i in range(a-1):
+    print(" "*i, end="")
+    print("*"*((a*2)-((i*2)+1)))
+
+for i in range(a):
+    print(" "*((a-i)-1), end="")
+    print("*"*((i*2)+1))
