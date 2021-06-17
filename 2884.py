d689625b74551f20317a2350a9d8517ad3b661f2
Gabiiii <anna9570@gmail.com>
2021-06-17 17:50:24 +0900

    2884 알람 시계

--- 2884.py
+++ 2884.py
@@ -1,12 +1,12 @@
 hour, min = map(int, input().split())
 
 if(min >= 45):
-        min -= 45;
+        min -= 45
 else:
-        if(hour == 0):
-                hour = 23
-        else:
-                hour -= 1
-        min += 15
+    if(hour == 0):
+        hour = 23
+    else:
+        hour -= 1
+    min += 15
 
 print(hour, min)
